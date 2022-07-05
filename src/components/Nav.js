8726d831afd52c41581ae1bd6a6f54210d37534d
EcaCosca <enriquecoscarelli@gmail.com>
2022-07-05 08:46:09 -0300

    Debugging

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,9 +5,9 @@ function Nav() {
   return (
     <div>
         <NavLink to='/' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Home </NavLink>
-        <NavLink to='/alex' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Home </NavLink>
-        <NavLink to='/vanessa' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Home </NavLink>
-        <NavLink to='/darshana' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Home </NavLink>
+        <NavLink to='/alex' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Alex </NavLink>
+        <NavLink to='/vanessa' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Vanessa </NavLink>
+        <NavLink to='/darshana' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Darshana </NavLink>
         <NavLink to='/jumps' style={({ isActive }) => ({color: isActive ? "green" : "blue"})}> Jumps </NavLink>
     </div>
   )
